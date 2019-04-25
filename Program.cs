8594ee3386176a8223c6e088d1adfb0dde655c73
Noe <VaescoNoe@gmail.com>
2019-04-25 00:09:51 -0500

    Throw

--- Program.cs
+++ Program.cs
@@ -6,19 +6,45 @@ namespace ClaseChecked
     {
         static void Main(string[] args)
         {
-            int entero = int.MaxValue;
+            try
+            {
+                switch (int.Parse(Console.ReadLine()))
+                {
+                    case 1:
+                        Console.WriteLine("I");
+                        break;
+                    case 2:
+                        Console.WriteLine("II");
+                        break;
+                    case 3:
+                        Console.WriteLine("III");
+                        break;
+                    case 4:
+                        Console.WriteLine("IV");
+                        break;
+                    case 5:
+                        Console.WriteLine("V");
+                        break;
+                    default:
+                        throw new InvalidOperationException("El dato no es valido");
 
+                }
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("No se permiten letras");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Finally siempre se executa");
+            }
+          
 
-            //checked: comprobacion de operaciones aritmeticas en numeros enteros
-            //{
-            //    entero++;
-            //}
-
-            entero = checked(entero++);
-
-            Console.WriteLine(entero);
             Console.ReadLine();
-
         }
     }
 }
