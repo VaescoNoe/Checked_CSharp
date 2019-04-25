a3f2ac150838bcdd32536c3c4be9203f65457e4e
Noe <VaescoNoe@gmail.com>
2019-04-25 00:07:51 -0500

    Checked

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ClaseChecked
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int entero = int.MaxValue;
+
+
+            //checked: comprobacion de operaciones aritmeticas en numeros enteros
+            //{
+            //    entero++;
+            //}
+
+            entero = checked(entero++);
+
+            Console.WriteLine(entero);
+            Console.ReadLine();
+
+        }
+    }
+}
